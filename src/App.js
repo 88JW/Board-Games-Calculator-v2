4339fd90f224ddd6596eee4c09ad892678d7cf5c
Wojciech Jaskula <w.jaskula8@gmail.com>
2023-08-27 20:22:28 +0200

    wersjonowanie

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import React from "react";
 import "./App.css";
 import NaviApp from "./components/pages/NaviApp";
 
-function App() {  
+function App() {
   return (
-     <div className="App">
+    <div className="App">
       <div className="Inner">
-        
-      <header  className="App-header">
-        <h1>Board Game Calculator</h1>
-      </header>
-      <NaviApp />
-     </div>
-    
+
+        <header className="App-header">
+          <h1>Board Game Calculator</h1>
+        </header>
+        <NaviApp />
+      </div>
+      <p>wersja z dnia 27.08.2023 20:22</p>
+
     </div>
   );
 }
