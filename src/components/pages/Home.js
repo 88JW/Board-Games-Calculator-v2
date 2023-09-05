881c27b06b2e4e4a895e70a84694980e6b2eecdd
Wojciech Jaskula <w.jaskula8@gmail.com>
2023-09-05 13:35:26 +0200

    oisanie wersji gry

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { MDBFooter,  } from 'mdb-react-ui-kit';
+import { MDBFooter, } from 'mdb-react-ui-kit';
 function Home() {
   return (
     <div>
       <p>Witaj w aplikacji do liczenia punktów w grach planszowych. Aplikacja jest w fazie rozwoju, więc proszę o wyrozumiałość.</p>
       <MDBFooter className='text-center text-white footer-own' style={{ backgroundColor: '#21081a' }}>
-        <p>wersja z dnia 28.08.2023 19:01</p>
+        <p>wersja z dnia 04.09.2023 16:30</p>
       </MDBFooter>
     </div>
   );
