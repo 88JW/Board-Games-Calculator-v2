8dcd6a4c5150ab8aa2e7fbc2c3183784fd08fc74
Wojciech Jaskula <w.jaskula8@gmail.com>
2023-09-10 20:03:43 +0200

    dodana lista przyszłych zmian:

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,7 +5,19 @@ function Home() {
     <div>
       <p>Witaj w aplikacji do liczenia punktów w grach planszowych. Aplikacja jest w fazie rozwoju, więc proszę o wyrozumiałość.</p>
       <MDBFooter className='text-center text-white footer-own' style={{ backgroundColor: '#21081a' }}>
-        <p>wersja z dnia 04.09.2023 16:30</p>
+        <p>elementy które zostaną dodane w najbliższym czasie:</p>
+        <ol>
+          <li>timer na ruch</li>
+          <li>Ruletka</li>
+          <li>Półk</li>
+        </ol>
+        <p>do zmiany:</p>
+        <ol>
+          <li>zmiana kolorów czcionek</li>
+          <li>zmiana fontu czcionek</li>
+          <li>zmiana kolorystyki tła i elementów generowanych</li>
+        </ol>
+        <p>wersja z dnia 10.09.2023 20:00</p>
       </MDBFooter>
     </div>
   );
